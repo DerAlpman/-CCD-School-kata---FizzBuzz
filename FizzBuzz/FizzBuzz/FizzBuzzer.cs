802d0cefa00b361089714c5bb300ce426883d91b
Stefan Alpers <stefan.alpers@gmx.net>
2019-04-05 22:07:22 +0200

    kata finished

---
+++ FizzBuzz/FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    internal static class FizzBuzzer
+    {
+        #region CONSTS
+
+        private const int FIZZ = 3;
+        private const int BUZZ = 5;
+
+        #endregion
+
+        #region METHODS
+
+        internal static IEnumerable<string> FizzBuzzRange(int min, int max, bool variant = false)
+        {
+            for (int i = min; i <= max; i++)
+            {
+                if (IsFizzBuzz(i))
+                {
+                    yield return "FizzBuzz";
+                }
+                else if (IsFizz(i, variant))
+                {
+                    yield return "Fizz";
+                }
+                else if (IsBuzz(i, variant))
+                {
+                    yield return "Buzz";
+                }
+                else
+                {
+                    yield return i.ToString();
+                }
+            }
+        }
+
+        private static bool IsFizzBuzz(int i)
+        {
+            return IsFizz(i) && IsBuzz(i);
+        }
+
+        private static bool IsBuzz(int i, bool variant = false)
+        {
+            return i % BUZZ == 0 || i.ToString().Contains(BUZZ.ToString());
+        }
+
+        private static bool IsFizz(int i, bool variant = false)
+        {
+            return i % FIZZ == 0 || i.ToString().Contains(FIZZ.ToString());
+        }
+
+        #endregion
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in FizzBuzzer.FizzBuzzRange(1, 100))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+            foreach (var item in FizzBuzzer.FizzBuzzRange(1, 100, true))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
